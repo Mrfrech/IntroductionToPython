462b07bdc6a79377eb82bc111a1d22b0d55b15e4
Mrfrech <dakshbharalgen@gmail.com>
2023-05-22 16:49:18 +0530

    added intro to var

---
+++ PythonforBeginners/var/var.py
@@ -0,0 +1,6 @@
+# variable in python are the set of words that we use to assin value to it.
+
+# some example 
+
+name = "mrfrech" # This is a 'string datatype' the value that we had assined
+print(name)
