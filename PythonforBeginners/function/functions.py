abb9e6fdcf07342713977bfcb5bbcd6cff9cf3ca
Mrfrech <dakshbharalgen@gmail.com>
2023-05-22 20:43:06 +0530

    added func.py file

---
+++ PythonforBeginners/function/functions.py
@@ -0,0 +1,24 @@
+# In this section we are gonna learn about fumctions 
+# fuctions are the block of code that we an use we its invoke or calls 
+
+# By using functions we can reduce our work by reusing of this block of code several times
+# Some examples
+
+a = 3
+
+def helloworld() : # Defining of function
+    if a > 2 :
+        print('helloWorld')
+
+helloworld() # Invoking of function
+
+
+
+
+data = "mrfrech"
+
+def helloworld2() :
+    if type(data) == str : # type() is pre-defined function that tells us type of data
+        print('This is string datatype')
+
+helloworld2()
